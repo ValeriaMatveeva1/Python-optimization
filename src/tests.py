2574ff9d66c2b4fad70311eb0c6ac80b7b89aabf
ValeriaMatveeva1 <matveevavalerochka11@gmail.com>
2022-03-29 20:21:56 +0300

    Lists of method's running time and comparison of them

---
+++ src/tests.py
@@ -0,0 +1,176 @@
+import random
+
+import algorithms as al
+import algorithms_jit as alj
+import algorithms_cython as alc
+import time
+import csv
+
+
+# умножение матриц (обычные списки)
+def test_1_1(n):
+    a = al.generate_rand_matrix_1(n)
+    b = al.generate_rand_matrix_1(n)
+    st = time.time()
+    al.matrix_mul_py(a, b)
+    return time.time() - st
+
+
+# умножение матриц (numpy-матрицы и умножение np.mul)
+def test_1_2(n):
+    a = al.generate_rand_matrix_2(n)
+    b = al.generate_rand_matrix_2(n)
+    st = time.time()
+    al.matrix_mul_numpy(a, b)
+    return time.time() - st
+
+
+# умножение матриц (jit и обычные списки)
+def test_1_3(n):
+    a = alj.generate_rand_matrix(n)
+    b = alj.generate_rand_matrix(n)
+    st = time.time()
+    alj.matrix_mul(a, b)
+    return time.time() - st
+
+
+# умножение матриц (cython и обычные списки)
+def test_1_4(n):
+    a = alc.generate_rand_matrix(n)
+    b = alc.generate_rand_matrix(n)
+    st = time.time()
+    alc.matrix_mul(a, b)
+    return time.time() - st
+
+
+# множество Мандельброта (обычная реализация)
+def test_2_1(n):
+    st = time.time()
+    al.mandelbrot_set(n)
+    return time.time() - st
+
+
+# множество Мандельброта (jit реализация)
+def test_2_2(n):
+    st = time.time()
+    res = alj.mandelbrot_set(n)
+    return time.time() - st
+
+
+# множество Мандельброта (cython реализация)
+def test_2_3(n):
+    st = time.time()
+    alc.mandelbrot_set(n)
+    return time.time() - st
+
+
+# нахождение всех простых чисел до n (обычная реализация)
+def test_3_1(n):
+    st = time.time()
+    al.find_primes(n)
+    return time.time() - st
+
+
+# нахождение всех простых чисел до n (jit реализация)
+def test_3_2(n):
+    st = time.time()
+    alj.find_primes(n)
+    return time.time() - st
+
+
+# нахождение всех простых чисел до n (cython реализация)
+def test_3_3(n):
+    st = time.time()
+    alc.find_primes(n)
+    return time.time() - st
+
+
+# метод жордана (более медленная реализация)
+def test_4_1(n):
+    a = al.generate_rand_matrix_1(n)
+    b = al.generate_rand_matrix_1(n)
+    st = time.time()
+    al.jordan_method_py(a, b)
+    return time.time() - st
+
+
+# метод жордана (хорошая реализация с numpy)
+def test_4_2(n):
+    a = al.generate_rand_matrix_2(n)
+    b = al.generate_rand_matrix_2(n)
+    st = time.time()
+    al.jordan_method_numpy(a, b)
+    return time.time() - st
+
+
+# метод жордана (jit реализация с numpy)
+def test_4_3(n):
+    a = al.generate_rand_matrix_2(n)
+    b = al.generate_rand_matrix_2(n)
+    st = time.time()
+    alj.jordan_method_py(a, b)
+    return time.time() - st
+
+
+# метод жордана (cython реализация)
+def test_4_4(n):
+    a = al.generate_rand_matrix_1(n)
+    b = al.generate_rand_matrix_1(n)
+    st = time.time()
+    alc.jordan_method_py(a, b)
+    return time.time() - st
+
+
+def write_tests(name: str, tests: list, b_l, b_r, step=1, rep=5):
+    with open(name, mode='w') as w_file:
+        file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+
+        for i in range(b_l, b_r + 1, step):
+            print(f"{round(100*(i-b_l)/(b_r-b_l))}%")
+            l = [i, ]
+            for f in tests:
+                t = 0
+                for j in range(rep):
+                    t += f(i)
+                t /= rep
+                l.append(t)
+            file_writer.writerow(l)
+
+
+def mean(name, new_name):
+    with open(name, mode='r') as r_file:
+        file_reader = csv.reader(r_file, delimiter=",", lineterminator="\r")
+        with open(new_name, mode='w') as w_file:
+            file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+            l1 = next(file_reader)
+            l2 = next(file_reader)
+            l3 = next(file_reader)
+            i = next(file_reader)
+            for i in file_reader:
+                l = [l1[0], ]
+                for j in range(1, len(l1)):
+                    l.append((float(l1[j]) + float(i[j]) + float(l2[j]) + float(l3[j])) / 4)
+                l1 = l2
+                l2 = l3
+                l3 = i
+                file_writer.writerow(l)
+
+
+# write_tests("data/matrix_mul.csv", [test_1_1, test_1_2, test_1_3, test_1_4], 1, 150)
+# print("1 OK")
+# write_tests("data/mandelbrot_set.csv", [test_2_1, test_2_2, test_2_3], 1, 150)
+# print("2 OK")
+# write_tests("data/find_primes.csv", [test_3_1, test_3_2, test_3_3], 10000, 11000)
+# print("3 OK")
+# write_tests("data/jordan_method.csv", [test_4_1, test_4_2, test_4_3, test_4_4], 1, 100)
+# print("4 OK")
+
+# mean("data/find_primes_o.csv", "data/find_primes.csv")
+
+alj.mandelbrot_set(200)
+st = time.time()
+res = alj.mandelbrot_set(200)
+print(time.time() - st)
+st = time.time()
+res = alc.mandelbrot_set(200)
+print(time.time() - st)
